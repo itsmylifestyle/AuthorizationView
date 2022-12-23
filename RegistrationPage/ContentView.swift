28f3bdcad4649219b01e15eb4a79cd4c4e282f92
itsmylifestyle <hahaii00@mail.ru>
2022-12-23 20:27:36 +0600

    Initial Commit

---
+++ RegistrationPage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RegistrationPage
+//
+//  Created by Айбек on 23.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RegistrationPage/RegistrationPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationPageApp.swift
+//  RegistrationPage
+//
+//  Created by Айбек on 23.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct RegistrationPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
